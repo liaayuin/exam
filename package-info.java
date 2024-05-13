9eba054ad276fddc30ac07ba4b090c60637d5090
liaayuin <lensayad23@gmail.com>
2024-05-13 17:19:13 +0300

    Add files via upload

--- package-info.java
+++ package-info.java
@@ -1,23 +1,25 @@
-package com.labQ6;
-class Main{
+package com.labQ4;
+class Mmain{
 	public static void main(String[] args) {
-		int[] array= {3, 7, 1, 9, 4};
-		int indexToDelete=7;
-		deletedElement(array,indexToDelete);
-	}
-
-	private static int[] deletedElement(int[] array, int indexToDelete) {
-		int[] newArray = new int[array.length - 1]; 
-		if(indexToDelete>0 && indexToDelete <array.length) {
-			for (int i = 0, j = 0; i < array.length; i++) { 
-				if (i != indexToDelete) { 
-					newArray[j++] = array[i]; 
-				}
-			}
+		char[] chars = {'a','A','B','C','d'};
+		for(int i=0; i<chars.length; i++) {
+			System.out.print(chars[i]+" ");
 		}
-		else{
-			new IllegalStateException("Invalid index " + array); 
-		return newArray;
 	}
+static void bubbleSort(int array[]) {
+    int size = array.length;
+    for (int i = 0; i < size - 1; i++)
+      for (int j = 0; j < size - i - 1; j++)
 
+        
+        if (array[j] > array[j + 1]) {
+
+          // swapping occurs if elements
+          // are not in the intended order
+          int temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+  }
 }
+  
